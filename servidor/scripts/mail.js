021bf26b2f2f3f64aee5a61c85f69706029dab11
Franco DAlessio <fmdo83@gmail.com>
2017-04-03 11:44:00 -0300

    Mail HTML

--- servidor/scripts/mail.js
+++ servidor/scripts/mail.js
@@ -1,21 +1,24 @@
 var helper = require('sendgrid').mail;
 //Cambiar por Key de sendgrid PROD.
 var sg = require('sendgrid')('');
+fs = require('fs');
 
 function sendMail(to_email, url)
 {
+  var message = fs.readFileSync('./extras/message.html', 'utf8');
   var to_email = new helper.Email(to_email);
   var from_email = new helper.Email("info@cdt.com");
   var subject = "CONSULTA MEDICA";
+  message = message.replace('URLVYDIO', url);
   //Content poner html del mail.
-  content = new helper.Content("text/plain", url);
+  content = new helper.Content("text/html", message);
   var mail = new helper.Mail(from_email, subject, to_email, content);
   var request = sg.emptyRequest({
     method: 'POST',
     path: '/v3/mail/send',
     body: mail.toJSON()
   });
-
+  //request.headers['Access-Control-Allow-Origin'] = '*'
   sg.API(request, function(error, response) {
     console.log(response.statusCode);
     console.log(response.body);
